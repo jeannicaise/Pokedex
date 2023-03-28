33ddf3606f7c4152e44055e013e90f46ddb680c0
Jean-Nicaise <jeannicaise2468@gmail.com>
2023-03-29 01:43:15 +0200

    pokedex quest 03 solved

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,30 @@
 import styles from './PokemonCard.module.css'
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
-    return (
+ 
+    let pokemon = pokemonList[0]
+    // pokemon = pokemonList[1]
+
+    return <div>{pokemon===pokemonList[0]? 
     <figure className={styles.card}>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image de bulbasaur "
+        <img src={pokemon.imgSrc} alt={pokemon.name}
         className={styles.cardImg}/>
-        <figcaption>bulbasaur</figcaption>
-    </figure>
-    );
+        <figcaption>{pokemon.name}</figcaption>
+    </figure>:<figure className={styles.card}> <p>???</p>   
+        <figcaption>{pokemonList[1].name}</figcaption></figure>}
+    </div>
+    ;
   }
   
   export default PokemonCard;
