3fe57355ea66d28f41cf33201ef3a0cf77d2d5da
Jean-Nicaise <jeannicaise2468@gmail.com>
2023-03-30 12:21:23 +0200

    Pokedex 05

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,20 @@
 import styles from './PokemonCard.module.css'
+import PropTypes from "prop-types";
 
 
+function PokemonCard({pokemon}) {
 
-function PokemonCard(props) {
- 
-
+    PokemonCard.propTypes = {
+        pokemon: PropTypes.shape({
+          name: PropTypes.string.isRequired,
+          imgSrc: PropTypes.string.isOptionnal,
+        }).isRequired,
+      }
     return(
     <figure className={styles.card}>
-        <img src={props.pokemon.imgSrc} alt={props.pokemon.name}
+        <img src={pokemon.imgSrc} alt={pokemon.name}
         className={styles.cardImg}/>
-        <figcaption>{props.pokemon.name}</figcaption>
+        <figcaption>{pokemon.name}</figcaption>
     </figure>
     )
     }
